395597430be50ad90ae937d6dfd997a59fa7201b
Juan Suarez <contato.jsuarez@gmail.com>
2023-04-04 10:57:36 -0300

    update user interface

--- LoopTune/LoopTune.py
+++ LoopTune/LoopTune.py
@@ -9,25 +9,25 @@ def reproduzir(mensagem):
 
 win = Tk()
 win.title("LoopTune")
-win.geometry("230x150")
+win.geometry("350x233")
 win.resizable(width=False, height=False)
 
-bg= PhotoImage(file = "lasalle.png")
+bg= PhotoImage(file = "lasalle1.png")
 label1 = Label( win, image = bg)
 label1.place(x = 0, y = 0)
 
-hourtxt = Label(win, text="Insira a(s) hora(s) que deseja reproduzir")
-hourtxt.grid(column=0, row=0)
-h = Entry(win,width=5)
-h.grid(column=0, row=1)
+#hourtxt = Label(win, text="Insira a(s) hora(s) que deseja reproduzir")
+#hourtxt.grid(column=0, row=0)
+h = Entry(win)
+h.place(x = 15, y = 140, width=20)
 
-mintxt = Label(win, text="Insira o(s) minuto(s) que deseja reproduzir")
-mintxt.grid(column=0, row=2)
+#mintxt = Label(win, text="Insira o(s) minuto(s) que deseja reproduzir")
+#mintxt.grid(column=0, row=2)
 m = Entry(win,width=5)
-m.grid(column=0, row=3)
+m.place(x = 45, y = 140, width=20)
 
 eta =Label(win, text="")
-eta.grid(column=0, row=5)
+eta.place(x = 15, y = 70, width=142)
 
 t = 0
 def loop():
@@ -45,6 +45,6 @@ def loop():
             
 
 botao = Button(win, text="Iniciar",command=loop) #,command=loop 
-botao.grid(column=0, row=4)
+botao.place(x =120 , y = 200, width=40)
        
 win.mainloop()
